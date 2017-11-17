cc3363507ffd76adc38d93cd1caffdf5b0db7b35
sujey <sujey_adelaydasweet@hotmail.com>
2017-11-17 16:56:26 -0600

    e

--- index.php
+++ index.php
@@ -1,66 +1,25 @@
-<?PHP  //TUNTORIALES//
-session_start();
-include "config.php";
-?>
-
-<form action="" method="post">
-
-  <p>Usuario
-    <input type="text" name="usuario" />
-  </p>
-  <p>Clave
-    <input type="password" name="password" />
-  </p>
-  <p>Repetir clave 
-    <input type="password" name="repassword" />
-  </p>
-  <p>
-    <input type="submit" name="enviar" value="Registrarme" />
-    
-        </p>
-</form>
-
-<?
-if(isset($_POST['enviar']))
-{
-if($_POST['usuario'] == '' or $_POST['password'] == '' or $_POST['repassword'] == '')
-{
-echo 'Debe llenar todos los campos por favor';
-}
-else
-{
-$sql = 'SELECT * FROM usuarios';
-$rec = mysql_query($sql);
-$verificar = 0;
-
-while($result = mysql_fetch_object($rec))
-{
-if($result->usuario == $_POST['usuario'])
-{
-$verificar = 1;
-}
-}
-if($verificar == 0)
-{
-if($_POST['password'] == $_POST['repassword'])
-{
-$usuario = $_POST['usuario'];
-$password = md5 ($_POST['password']);
-
-$sql = "INSERT INTO usuarios (usuario,password) VALUES ('$usuario','$password')";
-mysql_query($sql);
-
-echo 'Usted se registro con Èxito';
-}
-else
-{
-echo 'Las contraseÒas no coinciden';
-}
-}
-else
-{
-echo 'El usuario ya esta en nuestra base de datos';
-}
-}
-}
-?>
+<?php
+    session_start();
+    include('acceso_db.php');
+?> 
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+	    if(isset($_SESSION['usuario_nombre'])) {
+	?>
+	        Bienvenido: <a href="perfil.php?id=<?=$_SESSION['usuario_id']?>"><strong><?=$_SESSION['usuario_nombre']?></strong></a><br />
+	        <a href="logout.php">Cerrar Sesi√≥n</a>
+	<?php
+	    }else {
+	?>
+			<a href="registro.php">Registrarse</a> | <a href="acceso.php">Ingresar</a>
+	<?php
+	    }
+	?> 
+</body>
+</html>
